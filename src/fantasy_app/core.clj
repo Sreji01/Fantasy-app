6341621529fb8673287716e527d6382e6b9f3e4b
Sreji01 <marko.srejic01@gmail.com>
2024-12-11 21:35:53 +0100

    Added automaticly generated function: fetch-player-metrics.

--- src/fantasy_app/core.clj
+++ src/fantasy_app/core.clj
@@ -1,7 +1,30 @@
 (ns fantasy-app.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def players (atom []))
+
+;;Automaticly generated function
+(defn fetch-player-metrics []
+  (let [url "https://fantasy.premierleague.com/api/bootstrap-static/"
+        response (client/get url {:as :json})]
+    (if (= 200 (:status response))
+      (let [data (:body response)
+            player-elements (:elements data)
+            player-statistics (map (fn [player]
+                                     {:id (:id player)
+                                      :first-name (:first_name player)
+                                      :second-name (:second_name player)
+                                      :team (:team player)
+                                      :now-cost (/ (:now_cost player) 10.0)
+                                      :total-points (:total_points player)
+                                      :form (:form player)
+                                      :expected-points (:ep_next player)})
+                                   player-elements)]
+        (reset! players player-statistics))
+      (println "Failed to retrieve data from the API."))))
